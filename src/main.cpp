64cf655f6764261d6f028810ff690fd37a962c5d
Batirro <batirro34@protonmail.com>
2026-03-31 07:58:33 +0000

    feat: parse command-line parameters and handle run modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "Parameters.h"
 
-int main() {
-    std::cout << "Start\n";
+int main(int argc, char **argv) {
+    int result = Parameters::readParameters(argc - 1, argv + 1);
+    if (result != 0) {
+        std::cerr << "Błąd wczytywania parametrów." << std::endl;
+        return 1;
+    }
+
+    switch (Parameters::runMode) {
+        case Parameters::RunModes::help:
+        Parameters::help();
+        break;
+
+        case Parameters::RunModes::undefined:
+        default:
+            std::cerr << "Nie podano trybu działania. Użyj --help." << std::endl;
+            return 1;
+    }
     return 0;
 }

45f80ffc9960e925b6246160db6725c60c4df2cf
Batirro <batirro34@protonmail.com>
2026-04-15 15:55:00 +0000

    fix: integrate new version of Parameters library

--- src/modes/BenchmarkMode.cpp
+++ src/modes/BenchmarkMode.cpp
@@ -21,8 +21,7 @@ static std::string getAlgoName(Parameters::Algorithms algo) {
     switch(algo) {
         case Parameters::Algorithms::insertion: return "InsertionSort";
         case Parameters::Algorithms::quick: return "QuickSort";
-        // UWAGA: Używamy 'bubble' jako obejścia dla 'bucket', jeśli w bibliotece prowadzącego go brakuje
-        case Parameters::Algorithms::bubble: return "BucketSort (mapped from bubble)";
+        case Parameters::Algorithms::bucket: return "BucketSort";
         default: return "Unsupported/Unknown";
     }
 }
@@ -36,6 +35,16 @@ static std::string getStructName(Parameters::Structures str) {
     }
 }
 
+static std::string getDistName(Parameters::Distribution dist) {
+    switch(dist) {
+        case Parameters::Distribution::random: return "Random";
+        case Parameters::Distribution::ascending: return "Ascending";
+        case Parameters::Distribution::descending: return "Descending";
+        case Parameters::Distribution::ascending50Per: return "Ascending 50%";
+        default: return "Random/Unknown";
+    }
+}
+
 void BenchmarkMode::run() {
     if (Parameters::structureSize <= 0 || Parameters::iterations <= 0 || Parameters::resultsFile.empty()) {
         std::cerr << "Błąd: Brak wymaganych parametrów! (rozmiar struktury, liczba iteracji, plik wyjściowy)" << std::endl;
@@ -45,12 +54,23 @@ void BenchmarkMode::run() {
     std::cout << "Rozpoczynam badania (Benchmark Mode)..." << std::endl;
     std::cout << "Algorytm: " << getAlgoName(Parameters::algorithm) << std::endl;
     std::cout << "Struktura: " << getStructName(Parameters::structure) << std::endl;
+    std::cout << "Rozkład: " << getDistName(Parameters::distribution) << std::endl;
     std::cout << "Rozmiar instancji: " << Parameters::structureSize << std::endl;
     std::cout << "Liczba iteracji: " << Parameters::iterations << std::endl;
 
-    // Generujemy wspólną bazę danych dla wszystkich iteracji, aby zachować spójność badań (punkt 12 i 13 instrukcji)
+    // Generujemy wspólną bazę danych dla wszystkich iteracji
     Array baseArray(Parameters::structureSize);
-    baseArray.fillRandom();
+    
+    // Wpływ rozkładu (Badanie B)
+    if (Parameters::distribution == Parameters::Distribution::ascending) {
+        baseArray.fillSorted(true);
+    } else if (Parameters::distribution == Parameters::Distribution::descending) {
+        baseArray.fillSorted(false);
+    } else if (Parameters::distribution == Parameters::Distribution::ascending50Per) {
+        baseArray.fillPartiallySorted(50);
+    } else {
+        baseArray.fillRandom();
+    }
 
     std::vector<long long> times;
     times.reserve(Parameters::iterations);
@@ -65,14 +85,13 @@ void BenchmarkMode::run() {
             timer.start();
             if (Parameters::algorithm == Parameters::Algorithms::insertion) InsertionSort::sort(copy);
             else if (Parameters::algorithm == Parameters::Algorithms::quick) QuickSort::sort(copy);
-            else if (Parameters::algorithm == Parameters::Algorithms::bubble) BucketSort::sort(copy); // Tymczasowy hack
+            else if (Parameters::algorithm == Parameters::Algorithms::bucket) BucketSort::sort(copy);
             timer.stop();
             
             isSorted = Validator::isSorted(copy);
         } 
         else if (Parameters::structure == Parameters::Structures::singleList) {
             SinglyLinkedList copy;
-            // Przepisujemy od końca używając pushFront (bardzo szybkie O(1)) by zachować kolejność danych
             for (int j = Parameters::structureSize - 1; j >= 0; --j) {
                 copy.pushFront(baseArray.get(j));
             }
@@ -80,7 +99,7 @@ void BenchmarkMode::run() {
             timer.start();
             if (Parameters::algorithm == Parameters::Algorithms::insertion) InsertionSort::sort(copy);
             else if (Parameters::algorithm == Parameters::Algorithms::quick) QuickSort::sort(copy);
-            else if (Parameters::algorithm == Parameters::Algorithms::bubble) BucketSort::sort(copy);
+            else if (Parameters::algorithm == Parameters::Algorithms::bucket) BucketSort::sort(copy);
             timer.stop();
             
             isSorted = Validator::isSorted(copy);
@@ -94,7 +113,7 @@ void BenchmarkMode::run() {
             timer.start();
             if (Parameters::algorithm == Parameters::Algorithms::insertion) InsertionSort::sort(copy);
             else if (Parameters::algorithm == Parameters::Algorithms::quick) QuickSort::sort(copy);
-            else if (Parameters::algorithm == Parameters::Algorithms::bubble) BucketSort::sort(copy);
+            else if (Parameters::algorithm == Parameters::Algorithms::bucket) BucketSort::sort(copy);
             timer.stop();
             
             isSorted = Validator::isSorted(copy);
@@ -106,26 +125,23 @@ void BenchmarkMode::run() {
 
         times.push_back(timer.getMicroseconds());
 
-        // Pasek postępu co 10 iteracji
         if ((i + 1) % 10 == 0 || i == Parameters::iterations - 1) {
             std::cout << "Ukończono iterację: " << (i + 1) << " / " << Parameters::iterations << "\r" << std::flush;
         }
     }
     std::cout << std::endl;
 
-    // Obliczenia (Min, Max, Avg)
     long long minTime = *std::min_element(times.begin(), times.end());
     long long maxTime = *std::max_element(times.begin(), times.end());
     long long sumTime = 0;
     for (long long t : times) sumTime += t;
     double avgTime = static_cast<double>(sumTime) / Parameters::iterations;
 
-    // Zapis do CSV (punkt 10 instrukcji)
     std::ofstream csv(Parameters::resultsFile, std::ios::app);
     if (csv.is_open()) {
         csv.seekp(0, std::ios::end);
         if (csv.tellp() == 0) {
-            csv << "Date,Algorithm,Structure,Size,Iterations,MinTime_us,MaxTime_us,AvgTime_us\n";
+            csv << "Date,Algorithm,Structure,Distribution,Size,Iterations,MinTime_us,MaxTime_us,AvgTime_us\n";
         }
 
         auto now = std::chrono::system_clock::now();
@@ -134,6 +150,7 @@ void BenchmarkMode::run() {
         csv << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %H:%M:%S") << ","
             << getAlgoName(Parameters::algorithm) << ","
             << getStructName(Parameters::structure) << ","
+            << getDistName(Parameters::distribution) << ","
             << Parameters::structureSize << ","
             << Parameters::iterations << ","
             << minTime << ","
